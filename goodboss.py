eb93d2973da99fe66c7f1449b0a189628ed3c26a
athenova <aleksey.sergeyev@yandex.com>
2025-02-13 09:02:10 +0300

    Только утренняя

--- goodboss.py
+++ goodboss.py
@@ -23,10 +23,10 @@ def job(prompt):
     bot.send_message(chat_id=CHAT_ID, text=text, parse_mode="Markdown")
 
 schedule.every().day.at("09:00",'Europe/Moscow').do(job, prompt="Заряди команду на свершения на весь день")
-schedule.every().day.at("11:00",'Europe/Moscow').do(job, prompt="Позови команду на кофе")
-schedule.every().day.at("13:00",'Europe/Moscow').do(job, prompt="Напомни команде об обеде")
-schedule.every().day.at("16:00",'Europe/Moscow').do(job, prompt="Позови команду на чай")
-schedule.every().day.at("18:00",'Europe/Moscow').do(job, prompt="Напомни команде об окончании рабочего дня")
+#schedule.every().day.at("11:00",'Europe/Moscow').do(job, prompt="Позови команду на кофе")
+#schedule.every().day.at("13:00",'Europe/Moscow').do(job, prompt="Напомни команде об обеде")
+#schedule.every().day.at("16:00",'Europe/Moscow').do(job, prompt="Позови команду на чай")
+#schedule.every().day.at("18:00",'Europe/Moscow').do(job, prompt="Напомни команде об окончании рабочего дня")
 
 half_day = 12 * 60 * 60
 
