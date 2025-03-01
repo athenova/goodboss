4c947bf37675461bd7d50aaddaeb4467b492c112
athenova <aleksey.sergeyev@yandex.com>
2025-03-01 11:26:17 +0300

    Рефакторинг

--- goodboss.py
+++ goodboss.py
@@ -8,29 +8,26 @@ AI_TEXT_MODEL = 'deepseek-chat'
 CHAT_TOKEN_NAME = "DEEPSEEK_API_KEY"
 BOT_TOKEN_NAME = "ATHE_BOT_TOKEN"
 BOT_TOKEN = os.environ.get(BOT_TOKEN_NAME)
-# CHAT_ID = -1002374309134
+#CHAT_ID = -1002374309134
 CHAT_ID = '@KaroshiyNasyalnika'
 
-def job(prompt):
+def job(CHAT_ID=CHAT_ID):
     client = OpenAI(api_key=os.environ.get(CHAT_TOKEN_NAME), base_url="https://api.deepseek.com")
     text = client.chat.completions.create(
         model=AI_TEXT_MODEL,
         messages=[
             { "role": "system", "content": f"Ты - технический директор, лидер команды с 100% харизмой" },
-            { "role": "user", "content": prompt },
+            { "role": "user", "content": "Заряди команду на свершения на весь день, не используй 'Конечно'" },
         ]
     ).choices[0].message.content
     bot = telebot.TeleBot(BOT_TOKEN)
     bot.send_message(chat_id=CHAT_ID, text=text, parse_mode="Markdown")
 
-schedule.every().day.at("09:00",'Europe/Moscow').do(job, prompt="Заряди команду на свершения на весь день")
-#schedule.every().day.at("11:00",'Europe/Moscow').do(job, prompt="Позови команду на кофе")
-#schedule.every().day.at("13:00",'Europe/Moscow').do(job, prompt="Напомни команде об обеде")
-#schedule.every().day.at("16:00",'Europe/Moscow').do(job, prompt="Позови команду на чай")
-#schedule.every().day.at("18:00",'Europe/Moscow').do(job, prompt="Напомни команде об окончании рабочего дня")
+if __name__ == '__main__':
+    schedule.every().day.at("09:00",'Europe/Moscow').do(job)
 
-half_day = 12 * 60 * 60
+    half_day = 12 * 60 * 60
 
-for i in range(half_day):
-    schedule.run_pending()
-    time.sleep(1)
+    for i in range(half_day):
+        schedule.run_pending()
+        time.sleep(1)
---
+++ review.py
@@ -0,0 +1,5 @@
+from goodboss import job
+
+CHAT_ID = -1002374309134
+
+job(CHAT_ID=CHAT_ID)
